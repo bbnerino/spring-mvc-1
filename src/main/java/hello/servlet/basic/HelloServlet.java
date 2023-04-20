7876aec84dbb82a1aba4b36f3f57c3778e25fd27
임현홍 <bbnerino@gmail.com>
2023-04-20 16:17:14 +0900

    섹션 4 v2, v3 생성

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")// 서블릿 매핑 정보
 public class HelloServlet extends HttpServlet {
-    @Override// 서블릿이 호출되면 서비스 메서드가 호출된다.
+    @Override// 서블릿이 호출되면 서비스 메서드가 호출된다.+
     protected void service (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // HttpServletRequest, HttpServletResponse 는 HTTP 요청과 응답 정보를 편리하게 사용할 수 있는 기능을 제공한다.
         // HTTP 요청 메시지를 편리하게 조회할 수 있고, HTTP 응답 메시지를 편리하게 생성할 수 있다.
